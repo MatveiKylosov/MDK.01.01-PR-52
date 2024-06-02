42a6545b72ebf26354bd8ac16c64cf35b6a53ba2
MatveiKylosov <matveikyl@mail.ru>
2024-06-02 17:46:20 +0500

    Программирование окна Main. Kylosov

--- Pages/Main.xaml.cs
+++ Pages/Main.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
+﻿using ReportGeneration_Kylosov.Classes;
+using ReportGeneration_Kylosov.Items;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ReportGeneration_Kylosov.Pages
 {
@@ -20,9 +11,53 @@ namespace ReportGeneration_Kylosov.Pages
     /// </summary>
     public partial class Main : Page
     {
+        public List<GroupContext> AllGroups = GroupContext.AllGroups();
+        public List<StudentContext> AllStudents = StudentContext.AllStudents();
+        public List<WorkContext> AllWorks = WorkContext.AllWorks();
+        public List<EvaluationContext> AllEvaluations = EvaluationContext.AllEvaluations();
+        public List<DisciplineContext> AllDisciplines = DisciplineContext.AllDisciplines();
+
         public Main()
         {
             InitializeComponent();
+            CreateGroupUI();
+            CreateStudents(AllStudents);
+        }
+
+        public void CreateGroupUI()
+        {
+            foreach (GroupContext group in AllGroups)
+                CBGroups.Items.Add(group.Name);
+
+            CBGroups.Items.Add("Выберите");
+            CBGroups.SelectedIndex = CBGroups.Items.Count - 1;
+        }
+
+
+        public void CreateStudents(List<StudentContext> AllStudents)
+        {
+            Parent.Children.Clear();
+            foreach (StudentContext student in AllStudents)
+                Parent.Children.Add(new Items.Student(student, this));
+        }
+
+        private void SelectGroup(object sender, SelectionChangedEventArgs e)
+        {
+            if (CBGroups.SelectedIndex != CBGroups.Items.Count - 1)
+            {
+                int IdGroup = AllGroups.Find(x => x.Name == CBGroups.SelectedItem).Id;
+                CreateStudents(AllStudents.FindAll(x => x.IdGroup == IdGroup));
+            }
+        }
+        private void SelectStudents(object sender, KeyEventArgs e)
+        {
+            List<StudentContext> SearchStudent = AllStudents;
+            if (CBGroups.SelectedIndex != CBGroups.Items.Count - 1)
+            {
+                int IdGroup = AllGroups.Find(x => x.Name == CBGroups.SelectedItem).Id;
+                SearchStudent = AllStudents.FindAll(x => x.IdGroup == IdGroup);
+            }
+            CreateStudents(SearchStudent.FindAll(x => $"{x.Lastname} {x.Firstname}".Contains(TBFIO.Text)));
         }
     }
 }
